b169f8af64dd17c9bc64de731f040292edca96a7
Julian Gruber <julian@juliangruber.com>
2015-10-06 17:34:02 +0200

    bench

--- bench.js
+++ bench.js
@@ -4,14 +4,11 @@ var b = Buffer(30 * 1024 * 1024);
 b.fill('a');
 var lines = Math.ceil(b.length / 16);
 
-var dt = 0;
+var start = new Date;
 
 for (var i = 0; i < lines; i++) {
   var offset = i * 16;
-
-  var start = new Date;
   var entropy = calc(b, offset, offset + 16);
-  dt += (new Date) - start;
 }
 
-console.log('%sms', dt);
+console.log('%sms', (new Date) - start);
