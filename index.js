6be3705ac49e5a6afe2a94d52bb9d260c58bc535
Bence Dányi <bence@danyi.me>
2015-10-06 17:20:45 +0200

    perf

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function shannon(buf, start, end){
   end = end || buf.length;
   var count = end - start;
   var keys = [];
-  var C = {};
+  var C = [];
   var val;
 
   for (var i = start; i < end; i++) {
