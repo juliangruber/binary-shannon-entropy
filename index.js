6de5eca3f7141c96f19ff5af3f8e21d7d7dbaf37
Julian Gruber <julian@juliangruber.com>
2015-10-08 12:47:21 +0200

    implement perf suggestion by @jcollado

--- index.js
+++ index.js
@@ -20,13 +20,8 @@ function shannon(buf, start, end){
   }
 
   for (var i = 0; i < keys.length; i++) {
-    H += C[keys[i]] * log2(C[keys[i]]);
+    H += C[keys[i]] * Math.log(C[keys[i]]);
   }
 
-  return -H;
+  return -H / Math.LN2;
 }
-
-function log2(x){
-  return Math.log(x) / Math.LN2;
-}
-
